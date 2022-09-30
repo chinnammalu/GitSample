564c43d6650e36c8c6e0a08221d6adf858e8b5f1
chinnammalu <chinnammalupeddinti@gmail.com>
2022-09-30 12:41:20 +0530

    file is deleted

--- Employee.java
+++ Employee.java
@@ -1,8 +1,8 @@
 class Emplpyee{
-  
+   private String ename;
   pubic static void main(String args[]){
     
-    System.out.println("employee file");
+    System.out.println("employee file"+ename);
   }
   
 }
